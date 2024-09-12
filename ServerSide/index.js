7195cd5d299c636aabe662583e25ffce7430ff1c
mohamad akar <MoeAkar123456@gmail.com>
2024-09-12 12:52:47 +0300

    ready for deploy

--- ServerSide/index.js
+++ ServerSide/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:3000',''],
+    origin: ['http://localhost:3000','https://e-learning-sb94.onrender.com'],
     credentials: true
 }));
 
