0d82d9d293ad825455545dbda3777bcb8bdd4670
mohamad akar <MoeAkar123456@gmail.com>
2024-09-13 15:08:02 +0300

    last

--- ServerSide/Models/User.js
+++ ServerSide/Models/User.js
@@ -20,13 +20,12 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:true
     },
-    profilePhoto:{
-        url: String,
-        public_id: String,
-        default:{
-            url:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
-            publicId:null,
-        }
+    profilePhoto: {
+        url: { type: String,
+            default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" 
+        }, 
+        public_id: { type: String ,default:null}
+
     },
     isAdmin:{
         type:Boolean,
