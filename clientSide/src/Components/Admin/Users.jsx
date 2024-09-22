0b9888a52f91be8e1929b3a460ff0709095d44f4
mohamad akar <MoeAkar123456@gmail.com>
2024-09-22 20:15:15 +0300

    last

--- clientSide/src/Components/Admin/Users.jsx
+++ clientSide/src/Components/Admin/Users.jsx
@@ -10,7 +10,7 @@ const Users = () => {
     
     useEffect(() => {
         dispatch(getAllUsers())
-    }, [dispatch])
+    }, [])
 
     const handleDeleteUser = (userId) => {
         dispatch(deleteUser(userId)).then(() => {
